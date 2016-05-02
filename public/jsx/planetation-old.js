5e845cd74865265e4f76794c11485f19a32266ae
Michael Sturrus <msturrus@gmail.com>
2016-05-02 12:31:06 -0500

    trying to implement something to deal with flicker on change

--- public/jsx/planetation-old.js
+++ public/jsx/planetation-old.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import React3 from 'react-three-renderer';
-import THREE from 'three';
-import ReactDOM from 'react-dom';
-
-
-
-
-// var React     = require('react'),
-//     ReactDOM  = require('react-dom'),
-//     ReactTHREE = require('react-three'),
-//     THREE = require('three'),
-//
-//     _         = require('lodash');
+var React     = require('react'),
+    ReactDOM  = require('react-dom'),
+    ReactTHREE = require('react-three'),
+    THREE = require('three'),
+    _         = require('lodash');
 
 
 
@@ -237,7 +228,6 @@ var PlanetForm = React.createClass({
       //
       // },
       render: function(){
-
         document.getElementById('zone-container').innerHTML = ''
         var self = this
         var scene = new THREE.Scene();
@@ -364,82 +354,11 @@ var PlanetForm = React.createClass({
 
 
             </div>
-            {
-              this.state.planets.map(function(planet, i){
-                return <Planet albumCover={album.albumCover} album={album.albumName} artist={album.albumArtist} location={1} key={i} />
-              }.bind(this))
-            }
           </div>
 
         )
       }
     })
-
-    class Planet extends React.Component {
-      constructor(props, context) {
-        super(props, context);
-
-        // construct the position vector here, because if we use 'new' within render,
-        // React will think that things have changed when they have not.
-        this.cameraPosition = new THREE.Vector3(0, 0, 5);
-
-        this.state = {
-          cubeRotation: new THREE.Euler(),
-        };
-
-        this._onAnimate = () => {
-          // we will get this callback every frame
-
-          // pretend cubeRotation is immutable.
-          // this helps with updates and pure rendering.
-          // React will be sure that the rotation has now updated.
-          this.setState({
-            cubeRotation: new THREE.Euler(
-              this.state.cubeRotation.x + 0.1,
-              this.state.cubeRotation.y + 0.1,
-              0
-            ),
-          });
-        };
-      }
-
-      render() {
-        const width = window.innerWidth; // canvas width
-        const height = window.innerHeight; // canvas height
-
-        return (<React3
-          mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
-          width={width}
-          height={height}
-
-          onAnimate={this._onAnimate}
-        >
-          <scene>
-            <perspectiveCamera
-              name="camera"
-              fov={75}
-              aspect={width / height}
-              near={0.1}
-              far={1000}
-
-              position={this.cameraPosition}
-            />
-            <mesh
-              rotation={this.state.cubeRotation}
-            >
-              <sphereGeometry
-                width={1}
-                height={1}
-                depth={1}
-              />
-              <meshBasicMaterial
-                color={0x00ff00}
-              />
-            </mesh>
-          </scene>
-        </React3>);
-      }
-    }
 
     // {
     //   this.state.planets.map(function(planet, i){
@@ -467,7 +386,7 @@ var PlanetForm = React.createClass({
     //   }
     // })
 
-    ReactDOM.render(<PlanetForm/>, document.getElementById('example'))
+    ReactDOM.render(<PlanetForm/>, document.getElementById('dummy'))
 
     // material.map  = THREE.TextureLoader('mars_1k_color.jpg')
 
@@ -476,19 +395,3 @@ var PlanetForm = React.createClass({
     // <p>{this.state.album}</p>
     // <p>{this.state.artist}</p>
     // </div>
-
-
-// render: function() {
-//     var aspectratio = this.props.width / this.props.height;
-//     var cameraprops = {fov : 75, aspect : aspectratio,
-//                        near : 1, far : 5000,
-//                        position : new THREE.Vector3(0,0,600),
-//                        lookat : new THREE.Vector3(0,0,0)};
-//
-//     return <Renderer width={this.props.width} height={this.props.height}>
-//         <Scene width={this.props.width} height={this.props.height} camera="maincamera">
-//             <PerspectiveCamera name="maincamera" {...cameraprops} />
-//             <Cupcake {...this.props.cupcakedata} />
-//         </Scene>
-//     </Renderer>;
-//   }
